ea7b7afe009f9c57a655e6bae10f19ef090e1270
HanY0l0 <jan.surowiak@protonmail.com>
2024-08-15 09:15:58 +0200

    wyglad glownej

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Carousel from "./Carousel";
 import { data } from "./imgsForCarousel";
+import { Link } from "react-router-dom";
 // import { attraction } from "./AttractionsData";
 import "./Home.css";
 
 const Home = ({ attractions }) => {
   return (
     <div>
+      <div className="hero-section">
+        <h1>Balneario El Arenal</h1>
+        <h2>Vive la experiencia Arenal!</h2>
+        <Link to="/buy-tickets">
+          <button className="buy-tickets-button">Buy Tickets</button>
+        </Link>
+      </div>
+      <div className="header-section">
+        <h1>Balnearo El Arenal: a world of entertainment!</h1>
+      </div>
       <div className="carousel">
         <Carousel images={data} />
       </div>
